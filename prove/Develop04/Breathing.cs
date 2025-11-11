37b00dc335bae8b31744840c742c4dd7a22ad9f0
spencerallman <all21021@byui.edu>
2025-11-11 14:23:08 -0700

    Mindfulness Program Attempt - Build Error

---
+++ prove/Develop04/Breathing.cs
@@ -0,0 +1,12 @@
+public class Breathing : Activity
+{
+    public Breathing(string name, string description) : base(name, description)
+    {
+
+    }
+    
+    public void BreathCycle()
+    {
+        
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Breathing breathing = new Breathing("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflecting Activity");
+            Console.WriteLine("  3. Start Listening Activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    breathing.StartMessage();
+                    breathing.InputDuration();
+                    breathing.BreathCycle();
+                    breathing.EndMessage();
+                    break;
+
+                case "2":
+                    //do something
+                    break;
+
+                case "3":
+                    //do something
+                    break;
+
+                case "4":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice, try again.");
+                    break;
+            }
+            
+        }
     }
 }
