2840ec1e681096cc1f3449aaa4afc348635e6930
spencerallman <all21021@byui.edu>
2025-11-13 15:08:55 -0700

    deleted mindfulness program and will need to restart due to file errors

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,47 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Breathing breathing = new Breathing("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
-
-        bool running = true;
-
-        while (running)
-        {
-            Console.WriteLine("Menu Options:");
-            Console.WriteLine("  1. Start Breathing Activity");
-            Console.WriteLine("  2. Start Reflecting Activity");
-            Console.WriteLine("  3. Start Listening Activity");
-            Console.WriteLine("  4. Quit");
-            Console.Write("Select a choice from the menu: ");
-
-            string choice = Console.ReadLine();
-
-            switch (choice)
-            {
-                case "1":
-                    breathing.StartMessage();
-                    breathing.InputDuration();
-                    breathing.BreathCycle();
-                    breathing.EndMessage();
-                    break;
-
-                case "2":
-                    //do something
-                    break;
-
-                case "3":
-                    //do something
-                    break;
-
-                case "4":
-                    running = false;
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid choice, try again.");
-                    break;
-            }
-            
-        }
+        Console.WriteLine("Hello World");
     }
 }
