59bf039bba3101dddb4f6ad33c57affe3e6523bc
spencerallman <all21021@byui.edu>
2025-11-13 15:04:54 -0700

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(5, "yellow");
+        Rectangle rectangle = new Rectangle(6, 5, "red");
+        Circle circle = new Circle(3, "blue");
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+
+        foreach(Shape shape in shapeList)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
